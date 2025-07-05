a1d1b0ffdc29c84ced42c68cf9ae8fb167b199e0
Scott Green <skevingreen@gmail.com>
2025-07-05 11:41:24 -0700

    added RouterTestingModule to home component

--- ims-client/src/app/home/home.component.spec.ts
+++ ims-client/src/app/home/home.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HomeComponent } from './home.component';
 import { environment } from '../../environments/environment';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -10,7 +12,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent, HttpClientTestingModule]
+      imports: [HomeComponent, HttpClientTestingModule, RouterTestingModule]
     })
     .compileComponents();
 
